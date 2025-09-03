a3e5d3c71cc8438e6039a610d365141a5c0ed424
HLWeil <hl.weil@t-online.de>
2025-09-03 20:50:27 +0200

    some additional helper functions to retreive values from specific studies and assays

--- src/ARCtrl.QueryModel/Investigation.fs
+++ src/ARCtrl.QueryModel/Investigation.fs
@@ -40,6 +40,36 @@ module ArcInvestigationExtensions =
                 |> ResizeArray
                 |> ArcTables
 
+        /// Returns all values in the process sequence, that are connected to the given node and come before it in the sequence
+        ///
+        /// If a protocol name is given, returns only the values of the processes that implement this protocol
+        member this.PreviousValuesOf(node : QNode, ?ProtocolName, ?StudyName, ?AssayName) =
+            match StudyName, AssayName with
+            | Some s, Some a -> failwithf "Could not retreive previous of node %s: Cannot specify both StudyName and AssayName" node.Name
+            | Some s, None -> 
+                let previousNodes = ArcTables.getPreviousNodesBy node.Name this.ArcTables
+                let s = this.GetStudy(s)
+                previousNodes
+                |> List.collect (fun n -> s.PreviousValuesOf(n, ?ProtocolName = ProtocolName).Values)
+                |> ValueCollection
+            | None, Some a -> 
+                let previousNodes = ArcTables.getPreviousNodesBy node.Name this.ArcTables
+                let a = this.GetAssay(a)
+                previousNodes
+                |> List.collect (fun n -> a.PreviousValuesOf(n, ?ProtocolName = ProtocolName).Values)
+                |> ValueCollection               
+            | None, None ->
+                this.ArcTables.PreviousValuesOf(node.Name, ?ProtocolName = ProtocolName)
+
+        member this.PreviousCharacteristicsOf(node : QNode, ?ProtocolName, ?StudyName, ?AssayName) =
+            this.PreviousValuesOf(node, ?ProtocolName = ProtocolName, ?StudyName = StudyName, ?AssayName = AssayName).Characteristics()
+
+        member this.PreviousFactorsOf(node : QNode, ?ProtocolName, ?StudyName, ?AssayName) =
+            this.PreviousValuesOf(node, ?ProtocolName = ProtocolName, ?StudyName = StudyName, ?AssayName = AssayName).Factors()
+
+        member this.PreviousParametersOf(node : QNode, ?ProtocolName, ?StudyName, ?AssayName) =
+            this.PreviousValuesOf(node, ?ProtocolName = ProtocolName, ?StudyName = StudyName, ?AssayName = AssayName).Parameters()
+
     module Investigation =
 
         open Errors
